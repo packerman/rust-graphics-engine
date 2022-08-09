c2b278854c8684e78f8106177d5dcc55abcfe363
Piotr Ackermann <pioacker@gmail.com>
2022-08-09 22:29:06 +0200

    Uniform refactor (#7)

--- src/core/uniform.rs
+++ src/core/uniform.rs
@@ -1,24 +1,22 @@
-extern crate nalgebra_glm as glm;
-
 use anyhow::Result;
 use glm::{Vec3, Vec4};
 use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlUniformLocation};
 
-use super::gl::get_uniform_location;
+use super::gl;
 
 pub struct Uniform<T> {
     pub data: T,
     location: WebGlUniformLocation,
 }
 
-impl<T> Uniform<T> {
+impl<T: UploadData> Uniform<T> {
     pub fn new_with_data(
         context: &WebGl2RenderingContext,
         data: T,
         program: &WebGlProgram,
         name: &str,
     ) -> Result<Uniform<T>> {
-        let location = get_uniform_location(context, program, name)?;
+        let location = gl::get_uniform_location(context, program, name)?;
         let uniform = Uniform { data, location };
         Ok(uniform)
     }
@@ -30,35 +28,33 @@ impl<T> Uniform<T> {
         program: &WebGlProgram,
         name: &str,
     ) -> Result<()> {
-        self.location = get_uniform_location(context, program, name)?;
+        self.location = gl::get_uniform_location(context, program, name)?;
         Ok(())
+    }
+
+    pub fn upload_data(&self, context: &WebGl2RenderingContext) {
+        self.data.upload_data(context, &self.location);
     }
 }
 
 pub trait UploadData {
-    fn upload_data(&self, context: &WebGl2RenderingContext);
+    fn upload_data(&self, context: &WebGl2RenderingContext, location: &WebGlUniformLocation);
 }
 
-impl UploadData for Uniform<[f32; 3]> {
-    fn upload_data(&self, context: &WebGl2RenderingContext) {
-        context.uniform3fv_with_f32_array(Some(&self.location), &self.data);
+impl UploadData for [f32; 3] {
+    fn upload_data(&self, context: &WebGl2RenderingContext, location: &WebGlUniformLocation) {
+        context.uniform3fv_with_f32_array(Some(location), self);
     }
 }
 
-impl UploadData for Uniform<Vec3> {
-    fn upload_data(&self, context: &WebGl2RenderingContext) {
-        context.uniform3f(Some(&self.location), self.data.x, self.data.y, self.data.z);
+impl UploadData for Vec3 {
+    fn upload_data(&self, context: &WebGl2RenderingContext, location: &WebGlUniformLocation) {
+        context.uniform3f(Some(location), self.x, self.y, self.z);
     }
 }
 
-impl UploadData for Uniform<Vec4> {
-    fn upload_data(&self, context: &WebGl2RenderingContext) {
-        context.uniform4f(
-            Some(&self.location),
-            self.data.x,
-            self.data.y,
-            self.data.z,
-            self.data.w,
-        );
+impl UploadData for Vec4 {
+    fn upload_data(&self, context: &WebGl2RenderingContext, location: &WebGlUniformLocation) {
+        context.uniform4f(Some(location), self.x, self.y, self.z, self.w);
     }
 }
