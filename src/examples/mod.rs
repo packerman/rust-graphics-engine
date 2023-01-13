dacdc9c1ba9385a5b3583e949c521fcfe7dab4ca
Piotr Ackermann <pioacker@gmail.com>
2023-01-13 22:39:31 +0100

    Refactor

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -13,7 +13,7 @@ pub mod e21_procedural_texture;
 pub mod e22_text_texture;
 pub mod e23_billboarding;
 pub mod e24_sprite_material;
-// pub mod e25_heads_up_display;
+pub mod e25_heads_up_display;
 // pub mod e26_render_to_texture;
 // pub mod e27_compound_effect;
 // pub mod e28_lights;
